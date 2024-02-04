5c1529e755ea09ebe0dec6f519873d4c7c79bbb7
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-04 22:00:31 +0200

    Added Services section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -132,6 +132,19 @@ const Home = () => {
             </section>
 
             <About />
+
+            <section>
+                <div className="container">
+                    <div className="xl:w-[470px] mx-auto">
+                        <h2 className="heading text-center">Our medical services</h2>
+                        <p className="text__para text-center">
+                            World-class care for everyone. Our health System offers unmatched, expert health care.
+                        </p>
+                    </div>
+
+                    <ServiceList />
+                </div>
+            </section>
         </>
     );
 };
