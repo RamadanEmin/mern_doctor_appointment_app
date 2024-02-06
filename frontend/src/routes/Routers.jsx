b1b7931a0e889af72b9b46a5d9d9e9d7d61fd237
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-06 21:50:02 +0200

    Added Doctors page

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Services from '../pages/Services';
+import Doctors from '../pages/Doctors/Doctors';
 
 const Routers = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/services' element={<Services />} />
+            <Route path='/doctors' element={<Doctors />} />
         </Routes>
     );
 };
