38522b9c512e404d4ed5f1f3f6d95fdab588b492
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-06 20:22:54 +0200

    Added Services page

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Services from '../pages/Services';
 
 const Routers = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/services' element={<Services />} />
         </Routes>
     );
 };
