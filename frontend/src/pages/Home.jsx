62f8d84245a5ed2db7b2ff42c2701228ec110717
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-06 18:22:29 +0200

    Added Testimonial section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
 import FaqList from '../components/Faq/FaqList';
+import Testimonial from '../components/Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -226,6 +227,19 @@ const Home = () => {
                             <FaqList />
                         </div>
                     </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="container">
+                    <div className="xl:w-[470px] mx-auto">
+                        <h2 className="heading text-center">What our patient say</h2>
+                        <p className="text__para text-center">
+                            World-class care for everyone. Our health System offers unmatched, expert health care.
+                        </p>
+                    </div>
+
+                    <Testimonial/>
                 </div>
             </section>
         </>
