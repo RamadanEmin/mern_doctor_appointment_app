8f111705ead538e93fd150e9103a40393584d36b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-08 00:01:45 +0200

    Added Login page

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Services from '../pages/Services';
 import Contact from '../pages/Contact';
@@ -10,6 +11,7 @@ const Routers = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Signup />} />
             <Route path='/services' element={<Services />} />
             <Route path='/contact' element={<Contact />} />
