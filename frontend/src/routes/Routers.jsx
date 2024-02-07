643215b3ef619a353267e6aa13b4a8197d65e00f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-07 20:44:19 +0200

    Added Contact page

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Services from '../pages/Services';
+import Contact from '../pages/Contact';
 import Doctors from '../pages/Doctors/Doctors';
 import DoctorDetails from '../pages/Doctors/DoctorDetails';
 
@@ -9,6 +10,7 @@ const Routers = () => {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
             <Route path='/doctors' element={<Doctors />} />
             <Route path='/doctors/:id' element={<DoctorDetails />} />
         </Routes>
