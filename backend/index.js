c542c853c3079e1dd9c33f0d1fc0dea42b27a6e3
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-09 16:30:58 +0200

    Added register controller

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoute from './Routes/auth.js';
 
 dotenv.config();
 
@@ -12,10 +13,6 @@ const port = process.env.PORT || 8000;
 const corsOptions = {
     origin: true
 };
-
-app.get('/', (req, res) => {
-    res.send('Api is working');
-});
 
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
@@ -31,6 +28,7 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute);
 
 app.listen(port, () => {
     connectDB();
