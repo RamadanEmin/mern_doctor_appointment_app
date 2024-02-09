68229fd1607c51d7860d637336939e81fdfd04f2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-09 19:12:10 +0200

    Added login controller

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,6 +1,13 @@
 import User from '../models/UserSchema.js';
 import Doctor from '../models/DoctorSchema.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+
+const generateToken = (user) => {
+    return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET_KEY, {
+        expiresIn: '15d'
+    });
+};
 
 export const register = async (req, res) => {
     const { name, email, password, role, photo, gender } = req.body;
@@ -47,5 +54,41 @@ export const register = async (req, res) => {
         res.status(201).json({ success: true, message: 'User successfully created' });
     } catch (err) {
         res.status(500).json({ success: false, message: 'Internal server error, Try again' });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        let user = null;
+
+        const patient = await User.findOne({ email });
+        const doctor = await Doctor.findOne({ email });
+
+        if (patient) {
+            user = patient;
+        }
+        if (doctor) {
+            user = doctor;
+        }
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(400).json({ success: false, message: 'Invalid credential' });
+        }
+
+        const token = generateToken(user);
+
+        const { password, role, appointments, ...rest } = user._doc;
+
+        res.status(200).json({ success: true, message: 'Successfully login', token, data: { ...rest }, role });
+    } catch (err) {
+        res.status(500).json({ success: false, message: 'Failed to login' });
     }
 };
