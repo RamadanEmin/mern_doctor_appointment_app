0aa993f2f13dd26af809ad0ca9c288ff3f8f970a
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-10 08:30:17 +0200

    Added Authentication middleware

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getSingleUser, getAllUser } from '../Controllers/userController.js';
+import { authenticate } from '../auth/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:id', getSingleUser);
-router.get('/', getAllUser);
+router.get('/:id', authenticate, getSingleUser);
+router.get('/', authenticate, getAllUser);
 
 export default router;
