a307101f86b9a85e9635d7389e20e1fb14be463e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-10 20:15:28 +0200

    Added User account page

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,22 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup';
 import Services from '../pages/Services';
-import Contact from '../pages/Contact';
 import Doctors from '../pages/Doctors/Doctors';
 import DoctorDetails from '../pages/Doctors/DoctorDetails';
+import Contact from '../pages/Contact';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import MyAccount from '../Dashboard/user-account/MyAccount';
 
 const Routers = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Signup />} />
             <Route path='/services' element={<Services />} />
-            <Route path='/contact' element={<Contact />} />
             <Route path='/doctors' element={<Doctors />} />
             <Route path='/doctors/:id' element={<DoctorDetails />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/register' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/users/profile/me' element={<MyAccount />}/>
         </Routes>
     );
 };
