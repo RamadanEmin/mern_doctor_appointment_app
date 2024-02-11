a8eaa59cba6e44061e03eebe732dcf3dbbffb260
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-11 09:09:21 +0200

    Added GET User profile controller

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getSingleUser, getAllUser } from '../Controllers/userController.js';
+import { getSingleUser, getAllUser, getUserProfile } from '../Controllers/userController.js';
 import { authenticate } from '../auth/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/:id', authenticate, getSingleUser);
 router.get('/', authenticate, getAllUser);
+router.get('/profile/me', authenticate, getUserProfile);
 
 export default router;
