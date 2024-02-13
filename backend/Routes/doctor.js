63c3f2e338ef55c910cbbb1cccec7164e87fbff2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-13 19:22:39 +0200

    Added GET Doctor profile

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getSingleDoctor, getAllDoctor } from '../Contollers/doctorController.js';
+import {
+    getSingleDoctor,
+    getAllDoctor,
+    getDoctorProfile
+} from '../Controllers/doctorController.js';
+import { authenticate, restrict } from '../auth/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/:id', getSingleDoctor);
 router.get('/', getAllDoctor);
+router.get('/profile/me', authenticate, restrict(['doctor']), getDoctorProfile);
 
 export default router;
