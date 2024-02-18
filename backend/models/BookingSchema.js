064136bcf22063857de15751c9c2f3bbc4f8c2dc
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-18 14:38:45 +0200

    Added User population in Booking schema

--- backend/models/BookingSchema.js
+++ backend/models/BookingSchema.js
@@ -16,11 +16,6 @@ const bookingSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        appointmentDate: {
-            type: Date,
-            required: true
-
-        },
         status: {
             type: String,
             enum: ['pending', 'approved', 'cancelled'],
@@ -33,5 +28,14 @@ const bookingSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+bookingSchema.pre(/^find/, function (next) {
+    this.populate('user').populate({
+        path: 'doctor',
+        select: 'name'
+    });
+
+    next();
+})
 
 export default mongoose.model('Booking', bookingSchema);
